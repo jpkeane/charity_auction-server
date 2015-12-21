be91e7136eda636a3b70407e1cd7ab894aab7de2
James Keane <jpkeane@me.com>
2015-12-21 21:01:50 +0000

    Added Auction model

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :auction do
+    starts_at "2015-12-21 20:19:52"
+ends_at "2015-12-21 20:19:52"
+time_zone_id "MyString"
+physical_address "MyText"
+name "MyString"
+donation_window_ends_at "2015-12-21 20:19:52"
+  end
+
+end
